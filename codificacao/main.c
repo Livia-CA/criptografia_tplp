bf4d0b353727e3c084446a10e3b4f4bd60e73ade
Livia Caetano de Andrade <liviacaetanodeandrade@gmail.com>
2023-03-14 19:44:01 -0400

    adequa para a tabela de caracteres do trab

--- codificacao/main.c
+++ codificacao/main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
 #define TMNH 10000
 
 int main()
 {
-    char texto[TMNH];
+    char texto[TMNH], texto2[TMNH];
     int texto_num[2][TMNH], colunas, i, j, tmnh_texto, x;
 
     printf("entre com o texto que gostaria de criptografar: ");
@@ -15,24 +16,57 @@ int main()
 
     tmnh_texto = strlen(texto)-1;
 
+    for (i = 0; i < tmnh_texto; i++){
+        texto[i] = tolower(texto[i]);
+    }
+
     colunas = tmnh_texto%2 == 0 ? tmnh_texto/2 : tmnh_texto/2 + 1;
 
     x = 0;
 
     for (i = 0; i < 2; i++){
         for (j = 0; j < colunas; j++){
-            texto_num[i][j] = (double)texto[x];
+            if (texto[x] >= 'a' && texto[x] <= 'z'){
+                texto_num[i][j] = texto[x]-'a'+1;
+            } else if (texto[x] == '.') {
+                texto_num[i][j] = texto[x]-19;
+            } else if (texto[x] == ',') {
+                texto_num[i][j] = texto[x]-16;
+            } else {
+                texto_num[i][j] = 29;
+            }
             x++;
         }
     }
 
-    texto_num[1][colunas-1] = texto_num[1][colunas-1] == 10 ? 32 : texto_num[1][colunas-1];
+    x = 0;
+
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < colunas; j++){
+            if (texto_num[i][j] >= 1 && texto_num[i][j] <= 26){
+                texto2[x] = texto_num[i][j]+'a'-1;
+            } else if (texto_num[i][j] == 27) {
+                texto2[x] = texto_num[i][j]+19;
+            } else if (texto_num[i][j] == 28) {
+                texto2[x] = texto_num[i][j]+16;
+            } else {
+                texto2[x] = 32;
+            }
+            x++;
+        }
+    }
+
+    //texto_num[1][colunas-1] = texto_num[1][colunas-1] == 10 ? 29 : texto_num[1][colunas-1];
 
     for (i = 0; i < 2; i++){
         for (j = 0; j < colunas; j++){
             printf("%d ", texto_num[i][j]);
         }
         printf("\n");
+    }
+
+    for (i = 0; i < x; i++){
+        printf("%c ", texto2[i]);
     }
 
     return 0;
