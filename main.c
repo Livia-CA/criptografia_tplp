c67ff847220573f3e23e5fc66a07180fd78f3c54
Pedro Martins <phsmartins2004@gmail.com>
2023-03-18 12:22:38 -0400

    função do produto de matrizes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include "main.h"
 
-void gera_matriz(int matriz_a[2][2], int* determinante) {
+void gera_matriz(double matriz_a[2][2], int* determinante) {
     int i, j;
 
     srand(time(0));
@@ -25,7 +25,7 @@ void gera_matriz(int matriz_a[2][2], int* determinante) {
     }
 }
 
-void gera_matriz_adjunta(int matriz_a[2][2], int matriz_adjunta[2][2]) {
+void gera_matriz_adjunta(double matriz_a[2][2], int matriz_adjunta[2][2]) {
     int i, j, k, l;
 
     k = 1;
@@ -71,7 +71,7 @@ void converte_minusculo(char mensagem_usuario[TMNH], int tmnh_mensagem) {
     }
 }
 
-void converte_mensagem_para_numero(char mensagem_usuario[TMNH], int mensagem_numerica[2][TMNH], int qtd_colunas) {
+void converte_mensagem_para_numero(char mensagem_usuario[TMNH], double mensagem_numerica[2][TMNH], int qtd_colunas) {
     int i, j, k;
 
     k = 0;
@@ -92,17 +92,33 @@ void converte_mensagem_para_numero(char mensagem_usuario[TMNH], int mensagem_num
     }
 }
 
+void produto_matrizes(int qtd_colunas, double result[2][TMNH], double matriz_a[2][TMNH], double matriz_b[2][2]) {
+    int i, j, k;
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < qtd_colunas; j++){
+            result[i][j] = 0;
+            for(k = 0; k < 2; k++){
+                result[i][j] += matriz_a[k][j] * matriz_b[i][k];
+            }
+        }
+    }
+}
+
 int main() {
     int i, j;
-    int matriz_a[2][2];
+    double matriz_a[2][2];
     int determinante;
     int matriz_adjunta[2][2];
     double inversa_matriz_a[2][2];
 
     char mensagem_usuario[TMNH];
-    int mensagem_numerica[2][TMNH];
+    double mensagem_numerica[2][TMNH];
     int qtd_colunas;
     int tmnh_mensagem;
+
+    double mensagem_codificada[2][TMNH];
+    double mensagem_decodificada[2][TMNH];
 
     gera_matriz(matriz_a, &determinante);
 
@@ -110,7 +126,7 @@ int main() {
 
     for(i = 0; i < 2; i++) {
         for(j = 0; j < 2; j++) {
-            printf("%d ", matriz_a[i][j]);
+            printf("%f ", matriz_a[i][j]);
         }
         printf("\n");
     }
@@ -144,7 +160,29 @@ int main() {
 
     for (i = 0; i < 2; i++){
         for (j = 0; j < qtd_colunas; j++){
-            printf("%d ", mensagem_numerica[i][j]);
+            printf("%f ", mensagem_numerica[i][j]);
+        }
+        printf("\n");
+    }
+
+    produto_matrizes(qtd_colunas, mensagem_codificada, mensagem_numerica, matriz_a);
+
+    printf("\n\nMensagem codificada: \n");
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < qtd_colunas; j++){
+            printf("%f ", mensagem_codificada[i][j]);
+        }
+        printf("\n");
+    }
+
+    produto_matrizes(qtd_colunas, mensagem_decodificada, mensagem_codificada, inversa_matriz_a);
+
+    printf("\n\nMensagem decodificada: \n");
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < qtd_colunas; j++){
+            printf("%f ", mensagem_decodificada[i][j]);
         }
         printf("\n");
     }
--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #define TMNH 10000
 
-void gera_matriz(int matriz_a[2][2], int* determinante);
-void gera_matriz_adjunta(int matriz_a[2][2], int matriz_adjunta[2][2]);
+void gera_matriz(double matriz_a[2][2], int* determinante);
+void gera_matriz_adjunta(double matriz_a[2][2], int matriz_adjunta[2][2]);
 void gera_matriz_inversa(int matriz_adjunta[2][2], int determinante, double inversa_matriz_a[2][2]);
 
 void recebe_mensagem_usuario(char mensagem_usuario[TMNH]);
 void converte_minusculo(char mensagem_usuario[TMNH], int tmnh_mensagem);
-void converte_mensagem_para_numero(char mensagem_usuario[TMNH], int mensagem_numerica[2][TMNH], int qtd_colunas);
+void converte_mensagem_para_numero(char mensagem_usuario[TMNH], double mensagem_numerica[2][TMNH], int qtd_colunas);
+
+void produto_matrizes(int qtd_colunas, double result[2][TMNH], double matriz_a[2][TMNH], double matriz_b[2][2]);
