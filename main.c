f28aad6d71a98ecfbcc99f64ee91d61f8aeacc26
Pedro Martins <phsmartins2004@gmail.com>
2023-03-18 08:40:54 -0400

    função para gerar matriz adjunta

--- main.c
+++ main.c
@@ -23,10 +23,34 @@ void gera_matriz(int matriz_a[2][2]) {
     }
 }
 
+void gera_matriz_adjunta(int matriz_a[2][2], int matriz_adjunta[2][2]) {
+    int i, j, k, l;
+
+    k = 1;
+    l = 1;
+
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            if(i != j) {
+                matriz_adjunta[i][j] = -matriz_a[k][l];
+            } else {
+                matriz_adjunta[i][j] = matriz_a[k][l];
+            }
+
+            if(k == 1) {
+                k--;
+            }
+        }
+        k++;
+        l--;
+    }
+}
+
 int main() {
 
     int i, j;
     int matriz_a[2][2];
+    int matriz_adjunta[2][2];
 
     gera_matriz(matriz_a);
 
@@ -39,6 +63,8 @@ int main() {
         printf("\n");
     }
 
+    gera_matriz_adjunta(matriz_a, matriz_adjunta);
+    
     return 0;
 
 }
--- main.h
+++ main.h
@@ -1,1 +1,2 @@
 void gera_matriz(int matriz_a[2][2]);
+void gera_matriz_adjunta(int matriz_a[2][2], int matriz_adjunta[2][2]);
